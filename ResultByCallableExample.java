5670b253b167bde8d8eb03ccf0b33112af0dc84e
seoltaehyeok <83220871+seoltaehyeok@users.noreply.github.com>
2021-07-29 16:57:21 +0900

    ThreadPool ( Callable, ReturnType )
    리턴값이 있는 작업 완료 통보
    Callable 객체 생성
    
    Callable <T> task = new Callable<T> () { 	// <T> 타입 파라미터를 생성해야함
          @Override
          public T call() throws Exception {	// call 메소드는 T라는 리턴값을 가지고 있음
    	//스레드가 처리할 내용
    	return T; 			// 리턴 값 지정
          }
    };
    
    Future<T> future = executorService.submit(task)  //submit(task) 메소드를 사용하여 작업큐에 Callable 객체를 넣음
    
    try {				// call의 리턴값 T를 result에 저장
           T result = fure.get();		// 호출하게 되면 스레드가 call()을 모두 실행할 때까지 블록킹
    } catch (InterruptedException e) {
    	// 작업 처리 도중 interrupt 될 경우 실행코드
    } catch (ExecutionException e) {
    	// 작업 처리 도중 예외가 발생할 경우 실행코드
    }		
    
    
    submit은 future라는 객체를 리턴하고, 
    Future 타입에도 <T> 타입의 파라미터가 들어가 있으므로 Callable의 타입과 같아야함
    
    ex) Callable의 <T> 가 String 이라면 Future의 <T> 또한 String
         Callable의 <T> 가 Integer라면 Future의 <T> 또한 Integer 이어야 함
    -------------------------------------------------------------------------------------------------
    출력 :
    [작업 처리 요청]
    [처리 결과] : 55
    [작업 처리 완료]
    -------------------------------------------------------------------------------------------------
    즉, 결과값이 있는 타입을 정의할 때 Callable로 정의하고, 
    Callable의 타입 파라미터가 call()의 return 타입이고, Future의 타입 파라미터가 되며 future.get()의 리턴타입이 된다.

---
+++ ResultByCallableExample.java
@@ -0,0 +1,43 @@
+package ThreadPool_Blocking;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ResultByCallableExample {
+
+	public static void main(String[] args) {
+		ExecutorService executorService = Executors.newFixedThreadPool(
+				Runtime.getRuntime().availableProcessors() // 현재 컴퓨터 CPU의 코어의 수
+		);
+		
+		//작업처리요청
+		System.out.println("[작업 처리 요청]");
+		Callable<Integer> task = new Callable<Integer>() {
+			@Override
+			public Integer call() { // call()은 반드시 Callable에서 지정한 타입 파라미터와 동일해야함
+				int sum = 0;
+				for(int i=1; i<=10; i++) {
+					sum += i;
+				}
+				return sum;
+			}			
+		};
+		
+		//ThreadPool의 작업큐에 저장
+		//Future 객체는 나중에 get() 메소드로 얻을 수 있는 결과값이 Integer이므로 Future 또한 타입 파라미터<Integer> 지정
+		Future<Integer> future = executorService.submit(task); //submit은 리턴값 또한 받을 수 있음
+		
+		try {
+			int sum = future.get();  // future.get()은 위의 call() 메소드에서 리턴 된 sum 값을 받음
+			System.out.println("[처리 결과] : " + sum);
+			System.out.println("[작업 처리 완료]");
+		} catch (Exception e) {
+			System.out.println("[예외발생함] : " + e.getMessage());
+		}
+		
+		executorService.shutdown();
+	}
+
+}
